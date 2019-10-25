e179bdd2cea5d2d3ba37e6b383af2a17c26add49
--replace-all <--replace-all>
2019-10-24 21:12:23 -0500

    Added all

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-return;
+const express = require('express')
+const basicAuth = require('express-basic-auth')
+
+var app = express()
+
+app.use(express.json())
+app.use(basicAuth({
+    users: {'admin': 'ctrlalttec'},
+    challenge: true
+}))
+
+
+const fetch = require('node-fetch')
+
+
+/*
+async function getMails(){
+    return new Promise((resolve, reject)=>{
+        fetch('https://ctrl-alt-tec-members.herokuapp.com/members').then((data)=>data.json()).then((data)=>{
+            resolve(
+                data.map( l => ({
+                    address: l['CORREO ELECTRONICO '],
+                    name: l['NOMBRE (S)']
+                })  )
+            )
+        })
+    })
+}
+*/
+
+
+app.get('/', async (req, res)=>{
+    console.log(req.auth.user)
+    //let mails = await getMails()
+    //res.end(JSON.stringify(mails))
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+app.listen(process.env.PORT || 8080, function(){
+	console.log("Your node js server is running")
+})
